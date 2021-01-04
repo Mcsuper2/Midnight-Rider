955493786617cf401e7df337a7e76f1a252eb365
Marcus Yu <marcusyu478@gmail.com>
2021-01-04 11:32:16 -0800

    Jan 4 2021 Midnight Rider: Implemented status check and checked for usability.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 """
 CHOICES = """
     ----
+    E. Status Check
     Q. QUIT
     ----
 """
@@ -27,18 +28,38 @@ def intro():
     time.sleep(1)
 def main():
     intro()
+
+    # Variables
     done = False
+    km_travelled = 0           # 100 km is goal
+    agents_distance = -20.0
+    turns = 0                  # represents turns taken
+    tofu = 3                   # how much tofu is left, 3 is max capacity
+    fuel = 50                  # how much fuel is left, 50 is a full tank
+    hunger = 0                 # hunger increases with number of turns
+
+
     while not done:
         # TODO: Check if reached END GAME
         # Give the player their choices
         print(CHOICES)
         # Handle user's input
         users_choice = input("What do you want to do? ").lower().strip("!,.? ")
-        if users_choice == "q":
+        if users_choice == "e":
+            print(f"\t---Status Check---")
+            print(f"\tkm travelled {km_travelled} km")
+            print(f"\tFuel left: {fuel}")
+            print(f"\tAgents are {abs(agents_distance)} km behind you")
+            print(f"\t You have {tofu} left")
+            print("\t-----")
+            time.sleep(1)
+        elif users_choice == "q":
             done = True
         # TODO: Change the environment based on choice and RNG
-    # Outroduction
+    # OutroductionE
     print("Thanks for playing! Please play again. =)")
+
+
 if __name__ == '__main__':
     main()
 
