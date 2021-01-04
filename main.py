733b6f25817a199aac99891c665a7c0c36856944
Marcus Yu <marcusyu478@gmail.com>
2021-01-04 10:23:31 -0800

    Jan 4 2021 Midnight Rider

---
+++ main.py
@@ -0,0 +1,27 @@
+# main.py
+# Midnight Rider
+# A text adventure game that is riveting
+# IGN gives it 4/100
+
+
+def main():
+    pass
+
+    # Show introduction
+
+    # LOOP
+    # TODO: Check if reached END GAME
+
+    # TODO: Give the player their choices
+
+    # TODO: Change the environment based on choice and RNG
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
