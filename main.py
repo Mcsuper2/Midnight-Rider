f581ae836c2fa63b9b212b7650ff1030ccaf9aa2
Marcus Yu <marcusyu478@gmail.com>
2021-01-06 11:10:00 -0800

    Jan 6 2021 All options done.

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ REACH THE END BEFORE THE MAN GON GETCHU.
 """
 CHOICES = """
     ----
+    A. Eat a Tofu
+    B. Travel at a moderate speed
     C. Speed ahead at full throttle
     D. Stop for fuel at a refuelling station (No food available)
     E. Status Check
@@ -49,16 +51,55 @@ def main():
 
 
     while not done:
-        # TODO: Check if reached END GAME
+        # Check if reached END GAME
+        if km_travelled = MAX_DISTANCE TRAVELLED:
+            # WIN
+            #
         # Give the player their choices
         print(CHOICES)
         # Handle user's input
         users_choice = input("What do you want to do? ").lower().strip("!,.? ")
 
-        if users_choice == "c":
+        if users_choice == "a":
+            # Eat
+            if tofu > 0:
+                tofu -= 2
+                hunger = 0
+                print()
+                print("--------Mmmmm, soy bean goodness!")
+                print("--------Your hunger is sated.")
+                print(f"--------You have {tofu} remaining.")
+                print()
+            else:
+                print()
+                print("--------You have no tofu left, too bad, so sad.")
+                print()
+
+        elif users_choice == "b":
+            # Drive at a moderate speed
+            player_distance_now = random.randrange(6, 11)
+            agents_distance_now = random.randrange(7, 15)
+
+            # Burn fuel, less than option c
+            fuel -= random.randrange(2, 7)
+
+            # Player distance travelled
+            km_travelled += player_distance_now
+
+            # Agents distance travelled
+            agents_distance -= (player_distance_now - agents_distance_now)
+
+            # Give the user feedback
+            print("")
+            print(f"--------You sped ahead {player_distance_now} km!")
+            if hunger >= 30:
+                print("--------Your stomach is currently rumbling. Food is advised.")
+            print("")
+
+        elif users_choice == "c":
             # Drive fast
             player_distance_now = random.randrange(10, 16)
-            agents_distance_now = random.randrange (7, 15)
+            agents_distance_now = random.randrange(7, 15)
 
             # Burn more fuel than option b
             fuel -= random.randrange(5, 11)
@@ -72,8 +113,9 @@ def main():
             # Give the user feedback
             print("")
             print(f"--------You sped ahead {player_distance_now} km!")
+            if hunger >= 30:
+                print("--------Your stomach is currently rumbling. Food is advised.")
             print("")
-
 
         elif users_choice == "d":
             # Refuel
@@ -87,6 +129,8 @@ def main():
             print("")
             print("--------You filled the fuel tank.")
             print("--------The agents got closer...")
+            if hunger >= 30:
+                print("--------Your stomach is currently rumbling. Food is advised.")
             print("")
         elif users_choice == "e":
             print(f"\t---Status Check---")
@@ -98,6 +142,11 @@ def main():
             time.sleep(1)
         elif users_choice == "q":
             done = True
+
+        # Increase hunger
+        if users_choice not in ["a", "e"]:
+            hunger += random.randrange(5, 13)
+
         # TODO: Change the environment based on choice and RNG
     # Outroduction
     print("Thanks for playing! Please play again. =)")
